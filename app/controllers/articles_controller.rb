c8ef8a94698b457a24c33c57593258de69b2e3e7
Nata P <protasnb@yahoo.com>
2018-09-19 06:35:00 +0000

    Add edit, update show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,33 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def edit
+      @article = Article.find_by_id(params[:id])
+      if @article == nil
+        @article.errors.add(:id, 'does not exist')
+        flash[:notice] = "Article not found!" 
+      end
+  end
+  
+  def update
+    @article =  Article.find(params[:id])
+    if @article
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated!" 
+        redirect_to article_path(@article) 
+      else
+        render 'edit'
+      end
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
